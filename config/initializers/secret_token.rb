67635a1a1cae6a0cc4bccc8bd69f3397fef50962
Ivan Bastidas <ivan.bastidas@admios-sa.com>
2013-07-24 08:58:48 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AdmiosDeck::Application.config.secret_key_base = 'db2473df7f1c29e4fc4ad140e6c71847a5248da8ed29161d2a27ff75c7655e0a78158eb9c84c9575c356a03dca2ae46235f5c19408d8f541da7699cd3c79a25d'
